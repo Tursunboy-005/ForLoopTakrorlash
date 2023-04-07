10eaeba27984fd0f54b09431453d7f939eb90636
Tursunboy <tursunboyrahimjonov1@gmail.com>
2023-04-07 10:36:12 +0500

    uchburchar foe loop

--- ForLoopTakrorlash/ForLoopTakrorlash/Program.cs
+++ ForLoopTakrorlash/ForLoopTakrorlash/Program.cs
@@ -10,6 +10,18 @@ namespace ForLoopTakrorlash
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter number of row : ");
+            int row = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= row;i++)
+            {        
+                for(int j = 1; j <= row - i;j++)
+                {                   
+                        Console.Write(j + " ");                    
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
     }
 }
